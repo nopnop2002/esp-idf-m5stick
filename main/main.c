aa832a82fa38353fd5e2c54bb3249d99c38414a0
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-02-16 21:33:11 +0900

    Compatible with ESP-IDF V4

--- main/main.c
+++ main/main.c
@@ -112,4 +112,13 @@ void app_main(void)
 		vTaskDelay(40);
 	}
 #endif
+
+    // Restart module
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 }
