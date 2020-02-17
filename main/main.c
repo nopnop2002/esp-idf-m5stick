829475944b3636abb3dae466cd285affb8cf4822
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-02-17 20:52:17 +0900

    Update main.c

--- main/main.c
+++ main/main.c
@@ -113,12 +113,12 @@ void app_main(void)
 	}
 #endif
 
-    // Restart module
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+	// Restart module
+	for (int i = 10; i >= 0; i--) {
+		printf("Restarting in %d seconds...\n", i);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+	printf("Restarting now.\n");
+	fflush(stdout);
+	esp_restart();
 }
